4cbfb5b00a60ee639ab87f3bd0cb82f354692e43
Julio Soriano <jsoriano@aaa.com.do>
2016-10-10 11:15:12 -0400

    2016-10-10 this is a simple carousel jquery to test abilitys

---
+++ jquery-1.8.2/base.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("#ContentPlaceHolder1_docidentidad").attr("disabled", true);
+    $("#ContentPlaceHolder1_telefono,#ContentPlaceHolder1_fax").mask("(999) 999-9999");
+
+    $("#ContentPlaceHolder1_tipidentidad").change(function () {
+        if ($(this).val() != 0) {
+            $("#ContentPlaceHolder1_docidentidad").attr("disabled", false);
+
+            if ($(this).val() != 0 && $(this).val() == 1) {
+
+                $("#ContentPlaceHolder1_docidentidad").mask("9-99-9999999-9");
+
+            } else if ($(this).val() != 0 && $(this).val() == 2) {
+
+                $("#ContentPlaceHolder1_docidentidad").mask("999-9999999-9");
+
+            }
+        } else {
+            $("#ContentPlaceHolder1_docidentidad").attr("disabled", true);
+        }
+        
+    });
+    
+});
